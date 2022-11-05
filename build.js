f9e95fb15db03fd97ef308610355ab38c2f71212
hazel97 <omarmattia97@gmail.com>
2022-11-05 21:52:38 +0200

    added test building

--- build.js
+++ build.js
@@ -1,7 +1,12 @@
+import fs from 'fs';
+const testFiles = fs.readdirSync("./src/tests")
+.filter(file => file.endsWith("Spec.ts"))
+.map(file => "./src/tests/" + file);
+
 import("esbuild")
 .then((esbuild) => {
   esbuild.build({
-    entryPoints: ["src/index.ts"],
+    entryPoints: ["src/index.ts"].concat(testFiles),
     minify: true,
     platform: "node",
     outdir: "dist"
