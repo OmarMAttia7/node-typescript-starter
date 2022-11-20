7d74958f7fe8bfa9d6e1b6514d81f3bf05ff1385
hazel97 <omarmattia97@gmail.com>
2022-11-20 18:44:55 +0200

    test: Refactored testing functionality

--- src/tests/childSpec.ts
+++
@@ -1,9 +0,0 @@
-import { x } from "../index.js";
-
-export default () => {
-  describe("child suite", () => {
-    it("checks if child suites are working", () => {
-      expect(x).toEqual(1);
-    });
-  });
-};
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,10 +1,8 @@
 import { msg } from "../index.js";
-import childSpec from "./childSpec.js";
 describe("basic test", () => {
 
   it("checks if testing functionality is working", async () => {
     expect(msg).toEqual(msg);
   });
-  
-  childSpec();
+
 });
