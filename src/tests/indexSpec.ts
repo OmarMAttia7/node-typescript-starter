72b6ae5975f65fd8385ad5ae3e1f460ca8952906
hazel97 <omarmattia97@gmail.com>
2022-11-20 18:48:37 +0200

    test: Updated indexSpec.ts

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,7 +1,7 @@
 import { msg } from "../index.js";
 describe("basic test", () => {
 
-  it("checks if testing functionality is working", async () => {
+  it("checks if testing functionality is working", () => {
     expect(msg).toEqual(msg);
   });
 
